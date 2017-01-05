eb5e6a38824221d5683440532608c504d5af81a4
thegrims <grimshawaidan@gmail.com>
2017-01-05 03:57:21 -0800

    Requested html and css changes, initial lux bool integration

--- resi.store/js/index.js
+++ resi.store/js/index.js
@@ -1,48 +1,11 @@
 
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();   
-});
-
-/*@param url {String}
-@param method {String} HTTP verb ('GET', 'POST', 'DELETE', etc.)
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
-
-  } else if (typeof XDomainRequest != "undefined") {
-
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-
-  } else {
-
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-  return xhr;
-}
-
-var xhr = createCORSRequest('GET', url);
-if (!xhr) {
-  throw new Error('CORS not supported');
-}
-function bannerChange()
-    {
-        $.get("http://osurobotics.club/web_scripts/lux.bool", function( my_var ) {
-            // my_var contains whatever that request returned
-        });
-        
-        if (my_var==true)
-        {
-            console.log("true");
-        }
-    }
-
-    
+var xhttp = new XMLHttpRequest();
+xhttp.onload = function() {
+    //if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       var luxbool = xhttp.responseText;
+       console.log(luxbool);
+    //}
+};
+xhttp.open("GET", "lux.bool", true);
+xhttp.send();
