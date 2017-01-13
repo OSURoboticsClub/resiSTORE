e1ef3c23ec7da7cf90047c3ae0177234dbcf9b10
Aidan Grimshaw <grimshawaidan@gmail.com>
2017-01-12 18:08:01 -0800

    changed js slightly to fix jslint identified problems, made all http links https

--- resi.store/js/index.js
+++ resi.store/js/index.js
@@ -5,16 +5,14 @@ xhttp.onload = function() {
        // Typical action to be performed when the document is ready:
        var luxbool = xhttp.responseText;
     
-       if (luxbool=="1")
-       {
+       if (luxbool === "1") {
           document.getElementById("closed").style.visibility = "hidden";  
        }
-       if (luxbool=="0")
-       {
+       if (luxbool === "0") {
           document.getElementById("open").style.visibility = "hidden";  
        }
        console.log(luxbool);
     //}
-};
+}; //resi.store/web_scripts/
 xhttp.open("GET", "lux.bool", true);
 xhttp.send();
