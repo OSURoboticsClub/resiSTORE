0cdd0c1fadcf1b43a0375a672545515227ec9665
Aidan Grimshaw <grimshawaidan@gmail.com>
2017-01-12 20:29:41 -0800

    added updated event listener and cleaned out a little unneccesary comments
    also removed lux.bool because it is now on the server as a pointer

--- resi.store/js/index.js
+++ resi.store/js/index.js
@@ -1,18 +1,25 @@
 
-var xhttp = new XMLHttpRequest();
-xhttp.onload = function() {
-    //if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       var luxbool = xhttp.responseText;
-    
-       if (luxbool === "1") {
-          document.getElementById("closed").style.visibility = "hidden";  
-       }
-       if (luxbool === "0") {
-          document.getElementById("open").style.visibility = "hidden";  
-       }
-       console.log(luxbool);
-    //}
-}; //resi.store/web_scripts/
-xhttp.open("GET", "lux.bool", true);
-xhttp.send();
+function boolBanner () 
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        //if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           var luxbool = xhttp.responseText;
+
+           if (luxbool === "1") {
+              document.getElementById("closed").style.visibility = "hidden";  
+           }
+           if (luxbool === "0") {
+              document.getElementById("open").style.visibility = "hidden";  
+           }
+           console.log(luxbool);
+        //}
+    }; //resi.store/web_scripts/
+    xhttp.open("GET", "lux.bool", true);
+    xhttp.send();
+}
+function setUpTimer () {
+    window.setInterval(boolBanner,120000);
+}
+document.addEventListener('DOMContentLoaded', setUpTimer);
